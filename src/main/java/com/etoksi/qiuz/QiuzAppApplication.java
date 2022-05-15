619f8a1c50d0915ed46b35e4da7a266411b34169
etoksi <77209266+etoksi@users.noreply.github.com>
2022-05-15 20:15:44 +0300

    Initial commit

---
+++ src/main/java/com/etoksi/qiuz/QiuzAppApplication.java
@@ -0,0 +1,13 @@
+package com.etoksi.qiuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiuzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiuzAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etoksi/qiuz/QiuzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etoksi.qiuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiuzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
